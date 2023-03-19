d1cccabce374fa00870f3da57c84274b82bbdc64
ismail özdemir <ismail.zdemir.bm@gmail.com>
2023-03-19 21:50:31 +0300

    close #33

--- src/Services/ReportService/ReportService.Api/Program.cs
+++ src/Services/ReportService/ReportService.Api/Program.cs
@@ -1,6 +1,19 @@
+using ReportService.Application;
+using ReportService.Persistence;
+using Serilog;
+
+IConfiguration configuration = GetConfiguration();
+Log.Logger = CreateSerilogLogger(configuration);
+
+
+Log.Information("Configuring web application {ApplicationContext} ...", Program.AppName);
+
+try
+{
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -22,4 +35,45 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+    Log.Information("Starting web application {ApplicationContext}...", Program.AppName);
 app.Run();
+
+}
+catch (Exception ex)
+{
+
+    Log.Fatal(ex, "Program terminated unexpectedly {ApplicationContext}!", Program.AppName);
+
+}
+finally
+{
+    Log.CloseAndFlush();
+}
+
+
+
+Serilog.ILogger CreateSerilogLogger(IConfiguration configuration)
+{
+
+    return new LoggerConfiguration()
+        .MinimumLevel.Verbose()
+        .Enrich.WithProperty("ApplicationContext", Program.AppName)
+        .WriteTo.Console()
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+}
+
+IConfiguration GetConfiguration()
+{
+    var builder = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddEnvironmentVariables();
+
+    return builder.Build();
+}
+
+public partial class Program
+{
+    public const string AppName = "ReportService.Api";
+}
