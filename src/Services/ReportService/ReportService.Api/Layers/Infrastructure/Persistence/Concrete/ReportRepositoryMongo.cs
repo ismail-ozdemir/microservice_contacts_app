e12f6a1027f380b7600fc8d27fca94e294c6774f
İSMAİL ÖZDEMİR <ismail.ozdemir@netcad.com>
2023-03-20 20:04:04 +0300

    close #40

---
+++ src/Services/ReportService/ReportService.Api/Layers/Infrastructure/Persistence/Concrete/ReportRepositoryMongo.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using ReportService.Application.Abstractions.Repositories;
+using ReportService.Domain.Entities;
+using ReportService.Persistence.Contexts;
+
+namespace ReportService.Persistence.Concrete
+{
+    public class ReportRepositoryMongo : IReportRepository
+    {
+
+        private readonly ReportContextMongo _context;
+
+        public ReportRepositoryMongo(ReportContextMongo context)
+        {
+            _context = context;
+        }
+
+        public async Task<Guid> InsertReportAsync(Report report, CancellationToken cancellationToken)
+        {
+            await _context.Reports.InsertOneAsync(report);
+            return report.Id;
+        }
+    }
+
+
+
+
+}
